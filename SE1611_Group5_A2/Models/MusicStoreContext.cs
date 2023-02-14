3f4fb8f355f175cd041fc8e8d6412d30c7eb2635
Tuan Nguyen <nanhtuan1003@gmail.com>
2023-02-14 23:52:54 +0700

    screen 6 checkout

--- SE1611_Group5_A2/Models/MusicStoreContext.cs
+++ SE1611_Group5_A2/Models/MusicStoreContext.cs
@@ -29,7 +29,7 @@ namespace SE1611_Group5_A2.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =DESKTOP-KFBJJ26; database =MusicStore;uid=sa;pwd=123456;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("server =LAPTOP-1RCFEU2R; database =MusicStore;TrustServerCertificate=True;");
             }
         }
 
--- SE1611_Group5_A2/obj/Debug/net6.0-windows/App.g.i.cs
+++ SE1611_Group5_A2/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace SE1611_Group5_A2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SE1611_Group5_A2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             SE1611_Group5_A2.App app = new SE1611_Group5_A2.App();
             app.InitializeComponent();
